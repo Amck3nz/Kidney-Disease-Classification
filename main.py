f087f685b99e9133b694e8afd8a7e339a1eed6fd
Amck3nz <amckenzie18@outlook.com>
2023-12-06 13:55:15 -0500

    model training added

--- main.py
+++ main.py
@@ -1,3 +1,41 @@
-from src.cnnClassifier import logger
+from cnnClassifier import logger
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from cnnClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
 
-logger.info("Welcome to custom log:")
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>>>> Stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Prepare base model"
+try:
+    logger.info(f"********************")
+    logger.info(f">>>>> Stage {STAGE_NAME} started <<<<<")
+    obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Model training"
+if __name__ == '__main__':
+    try:
+        logger.info(f"********************")
+        logger.info(f">>>>> Stage {STAGE_NAME} started <<<<<")
+        obj = ModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>> Stage {STAGE_NAME} completed <<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
